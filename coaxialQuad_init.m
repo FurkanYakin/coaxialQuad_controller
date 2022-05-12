25569e8dd1ca8375db3219632cc596e8948c5f98
FurkanYakin <frknykn42@gmail.com>
2022-05-12 19:15:36 +0300

    Kontrolcu eklendi

--- coaxialQuad_init.m
+++ coaxialQuad_init.m
@@ -1,12 +1,12 @@
 %%
 %Quadcopter simulation variables
 
-%%
+%% Constants
 
-b=0.275;        %Thrust coefficent
-d=0.5;          %Drug coefficent
-m=3.750;        %Mass (kg)
-l=0.15;         %Arm length (meter)
+b=1.144e-8;        %Thrust coefficent
+d=9.94e-10;          %Drug coefficent
+m=0.284;        %Mass (kg)
+l=0.1;         %Arm length (meter)
 
 Ixx=1;
 Iyy=1;          %Inertia moments (kgm^2)
@@ -15,18 +15,35 @@ Izz=1;
 Ax=0;
 Ay=0;           %Air drag coeff in each axis
 
-K_emf=1;        %motor coefficent (Vs/rad)
-J=1;            %Motor mass moment of inertia (kgm^2)
-b_m=1;          %Motor damping coefficient (Nms)
-Kt=1;           %Motor torque coefficient (Nm/A)
-R=1;            %Motor rotor resistance (ohm)
-L=1;            %Motor rotor inductance (H)
+K_emf=0.00255;        %motor coefficent (Vs/rad)
+J=6.5e-7;            %Motor mass moment of inertia (kgm^2)
+b_m=2.415e-6;          %Motor damping coefficient (Nms)
+Kt=0.00255;           %Motor torque coefficient (Nm/A)
+R=0.117;            %Motor rotor resistance (ohm)
+L=1.17e-4;            %Motor rotor inductance (H)
 omega_max=inf;  %motor max limit
 omega_min=0;    %motor min limit
 
-V=1;            %Battary Voltage
-Ah=1;           %Battary capacity
-C=1;            %Battary C rate
+V=11.1;            %Battary Voltage
+Ah=0.7;           %Battary capacity
+C=60;            %Battary C rate
+
+%% PID Values
+Z_P = 5.8;
+Z_I = 0.5;
+Z_D = 2;
+
+phi_P = 2;
+phi_I = 2;
+phi_D = 0.1;
+
+theta_P = 2;
+theta_I = 2;
+theta_D = .1;
+
+psi_P = 0.1;
+psi_I = 0.1;
+psi_D = 0;
 
 %% Velocity input
 
