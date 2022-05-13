f1c91b35755c45a78362b4e2748df36dcf2ce750
FurkanYakin <frknykn42@gmail.com>
2022-05-14 00:36:31 +0300

    Matematik modelde bir hata var gibi. Sistem beklendiği gibi cevap vermiyor

--- coaxialQuad_init.m
+++ coaxialQuad_init.m
@@ -3,30 +3,30 @@
 
 %% Constants
 
-b=1.144e-8;        %Thrust coefficent
-d=9.94e-10;          %Drug coefficent
-m=0.284;        %Mass (kg)
-l=0.1;         %Arm length (meter)
+b=1.869E-7;         %Thrust coefficent
+d=1.706E-8;         %Drug coefficent
+m=3.1;            %Mass (kg)
+l=0.1;             %Arm length (meter)
 
-Ixx=1;
-Iyy=1;          %Inertia moments (kgm^2)
-Izz=1;
+Ixx=0.182;
+Iyy=0.188;              %Inertia moments (kgm^2)
+Izz=0.335;
 
 Ax=0;
-Ay=0;           %Air drag coeff in each axis
+Ay=0;               %Air drag coeff in each axis
 
-K_emf=0.00255;        %motor coefficent (Vs/rad)
-J=6.5e-7;            %Motor mass moment of inertia (kgm^2)
-b_m=2.415e-6;          %Motor damping coefficient (Nms)
-Kt=0.00255;           %Motor torque coefficient (Nm/A)
+K_emf=0.00255;      %motor coefficent (Vs/rad)
+J=6.5e-7;           %Motor mass moment of inertia (kgm^2)
+b_m=2.415e-6;       %Motor damping coefficient (Nms)
+Kt=0.00255;         %Motor torque coefficient (Nm/A)
 R=0.117;            %Motor rotor resistance (ohm)
-L=1.17e-4;            %Motor rotor inductance (H)
-omega_max=inf;  %motor max limit
-omega_min=0;    %motor min limit
+L=1.17e-4;          %Motor rotor inductance (H)
+omega_max=inf;      %motor max limit
+omega_min=0;        %motor min limit
 
-V=11.1;            %Battary Voltage
-Ah=0.7;           %Battary capacity
-C=60;            %Battary C rate
+V=11.1;             %Battary Voltage
+Ah=0.7;             %Battary capacity
+C=60;               %Battary C rate
 
 %% PID Values
 Z_P = 5.8;
@@ -39,7 +39,7 @@ phi_D = 0.1;
 
 theta_P = 2;
 theta_I = 2;
-theta_D = .1;
+theta_D = 0.1;
 
 psi_P = 0.1;
 psi_I = 0.1;
